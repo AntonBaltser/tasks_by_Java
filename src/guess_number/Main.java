8ff5a193a9f59b5b71fc36f7c25a50812e6e6500
AntonBaltser <antonbaltser@yahoo.com>
2022-04-22 13:48:12 +0300

    added range and number of attempts

--- src/guess_number/Main.java
+++ src/guess_number/Main.java
@@ -5,22 +5,33 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
-        int guess;
+        int guess, max, min, attempts;
         String agree;
         System.out.print("Möchten Sie in ein Spiel spielen, das man \"rate Zahl\" nennt? Tippen Sie [ yes / no ] ein: ");
         agree = scan.next();
+
         while (agree.equalsIgnoreCase("y")) {
-            int theNumber = (int) (Math.random() * 100 + 1);
+
+        System.out.println("Geben Sie Bereich der Zahlen von min bis max ein.");
+        System.out.print("MIN: ");
+        min = scan.nextInt();
+        System.out.print("MAX: ");
+        max = scan.nextInt();
+
+            int theNumber = (int) (Math.random() * (max-(min)) + (min));
             System.out.println(theNumber);
-            System.out.print("Tippen Sie bitte die Zahl von 1 bis 100 ein: ");
+            attempts = 1;
+            System.out.print("Tippen Sie bitte die Zahl in Ihrem Umfang ein: ");
            do  {
                 guess = scan.nextInt();
-                if (guess < theNumber) {
-                    System.out.print("Ihr vorausplanende Zahl ist " + guess + " WENIGER als benötigt. Versuchen Sie noch ein mal: ");
-                } else if (guess > theNumber) {
-                    System.out.print("Ihr vorausplanende Zahl ist " + guess + " GRÖßER als benötigt. Versuchen Sie noch ein mal: ");
-                } else
-                    System.out.println("Gratulation! Sie haben gewonnen!");
+                if (guess < theNumber) System.out.print("Ihr vorausplanende Zahl ist " + guess + " WENIGER als benötigt. Versuchen Sie noch ein mal: ");
+
+                else if (guess > theNumber) System.out.print("Ihr vorausplanende Zahl ist " + guess + " GRÖßER als benötigt. Versuchen Sie noch ein mal: ");
+
+                 else System.out.println("Gratulation! Sie haben gewonnen!Ihre Anzahl der Versuche: " + attempts);
+
+                    attempts++;
+
             }while (guess != theNumber);
             System.out.print("Haben Sie Lust, noch ein mal zu spielen? [ n / y] :");
             agree = scan.next();
